dfdc68311e1074d7ab37ac501452a56a6127eed6
Manuel <magini15@gmail.com>
2023-10-03 17:50:39 +0200

    pronti

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output:"export",
-  basePath: "/ProgettoNextFUTURA",
+  basePath: "/ProgettoNextFUTURA/articles",
   images:{
     unoptimized:true,
   }
